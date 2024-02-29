f30d04530c17c3f8f7bba8dbf3fe5f5c936e745f
Irene Zatti <irene.zatti16@gmail.com>
2024-02-29 17:46:38 +0100

    change in database

--- model/init_db.sql
+++ model/init_db.sql
@@ -1,24 +1,25 @@
-
-SET foreign_key_checks = 0;
-DROP TABLE if exists game;
-DROP TABLE if exists user;
-SET foreign_key_checks = 1;
-
-CREATE TABLE `games`(
-    `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    `Q1` TINYINT NOT NULL,
-    `Q2` TINYINT NOT NULL,
-    `Q3` TINYINT NOT NULL,
-    `Q4` TINYINT NOT NULL,
-    `Q5` TINYINT NOT NULL,
-    `user_id` BIGINT NOT NULL,
+drop table if exists game;
+drop table if exists quotes_info;
+drop table if exists user;
+CREATE TABLE `game`(
+    `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `user_id` BIGINT UNSIGNED NOT NULL,
     `game_total` INT NOT NULL
 );
+CREATE TABLE `quotes_info`(
+    `question_id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `quote_text` VARCHAR(255) NOT NULL,
+    `solution_char` VARCHAR(255) NOT NULL,
+    `user_answer` VARCHAR(255) NOT NULL,
+    `result_points` TINYINT NOT NULL,
+    `game_id` BIGINT UNSIGNED NOT NULL
+);
 CREATE TABLE `user`(
-    `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
     `total_points` INT NOT NULL,
     `user_name` VARCHAR(255) NOT NULL
 );
-
--- ALTER TABLE
---     `game` ADD CONSTRAINT `game_user_id_foreign` FOREIGN KEY(`user_id`) REFERENCES `user`(`id`);
+ALTER TABLE
+    `game` ADD CONSTRAINT `game_user_id_foreign` FOREIGN KEY(`user_id`) REFERENCES `user`(`id`);
+ALTER TABLE
+    `quotes_info` ADD CONSTRAINT `quotes_info_game_id_foreign` FOREIGN KEY(`game_id`) REFERENCES `game`(`id`);
